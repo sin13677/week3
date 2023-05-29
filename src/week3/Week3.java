e18070b7c36ae9c2db4846364daa6cc4700528a7
User <User@jasbir>
2023-05-29 13:44:27 -0400

    version 1

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -15,7 +15,11 @@ public class Week3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("learning github operations");
+         System.out.println("learning github push");
+          System.out.println("learning github pull operations");
+          //this is the branch      
+             
     }
     
 }

2530afe17495b29383f83cda2eddd869e2cc097a
User <User@jasbir>
2023-05-29 12:38:11 -0400

    version 1

---
+++ src/week3/Week3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week3;
+
+/**local rep - step 1. initialize versoining , .gitconfig, .gitignore, 4. commit
+ * 5. push the code to git hub remote repository
+ *
+ * @author jasbir singh
+ */
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
